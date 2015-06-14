b050c009cd69c538d0bb46632975e954165b977d
Steven McCord <steven.a.mccord@gmail.com>
2015-06-14 08:48:23 -0400

    updating remote db variables

--- confs/db.php
+++ confs/db.php
@@ -1,20 +1,7 @@
 <?php
 $host = $_SERVER["HTTP_HOST"];
-//production variables
-if ($host == "www.thebeachwebsite.com" || $host == "thebeachwebsite.com" || $host == "2015.thebeachwebsite.com"){
-	define('DB_NAME', 'heroku_c93eb6e13dfc629');
-	/** MySQL database username */
-	define('DB_USER', 'b25646bfd625db');
-	/** MySQL database password */
-	define('DB_PASSWORD', '9994db5d');
-	/** MySQL hostname */
-	define('DB_HOST', 'db.thebeachwebsite.com');
-	/** Database Charset to use in creating database tables. */
-	define('DB_CHARSET', 'utf8');
-	/** The Database Collate type. Don't change this if in doubt. */
-	define('DB_COLLATE', '');
 //local variables
-}else{
+if ($host == "www.localhost.com" || $host == "localhost"{
 	define('DB_NAME', 'urbanstalls');
 	/** MySQL database username */
 	define('DB_USER', 'wordpress');
@@ -22,6 +9,19 @@ if ($host == "www.thebeachwebsite.com" || $host == "thebeachwebsite.com" || $hos
 	define('DB_PASSWORD', 'smurfmurph');
 	/** MySQL hostname */
 	define('DB_HOST', '127.0.0.1');
+	/** Database Charset to use in creating database tables. */
+	define('DB_CHARSET', 'utf8');
+	/** The Database Collate type. Don't change this if in doubt. */
+	define('DB_COLLATE', '');
+//remote variables
+}else{
+	define('DB_NAME', 'heroku_c93eb6e13dfc629');
+	/** MySQL database username */
+	define('DB_USER', 'b25646bfd625db');
+	/** MySQL database password */
+	define('DB_PASSWORD', '9994db5d');
+	/** MySQL hostname */
+	define('DB_HOST', 'db.thebeachwebsite.com');
 	/** Database Charset to use in creating database tables. */
 	define('DB_CHARSET', 'utf8');
 	/** The Database Collate type. Don't change this if in doubt. */
